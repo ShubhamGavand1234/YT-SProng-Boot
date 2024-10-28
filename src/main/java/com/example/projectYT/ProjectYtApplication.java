659ea7fa47b57e24abecd37b427682f4a8521baa
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-10-28 23:55:59 +0530

    Basic Starting files

---
+++ src/main/java/com/example/projectYT/ProjectYtApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectYT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectYtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectYtApplication.class, args);
+	}
+
+}
